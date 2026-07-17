ab8f0cdcd1291696a1a6f817c06e8ff563a5db2e
Kreliannn <krelianquimson@gmail.com>
2026-07-17 14:42:03 +0800

    update

---
+++ model/documentRequest.model.ts
@@ -0,0 +1,30 @@
+import mongoose, { Schema } from 'mongoose';
+
+
+const DocumentSchema = new Schema({
+    resident: { type: mongoose.Schema.Types.ObjectId, ref: "Accounts", required: true },
+    document: { type: String, required: true },
+    status: { type: String, required: true },
+    isPaid : { type: Boolean, required: true },
+
+    fullName : { type: String, required: false },
+    contact: { type: String, required: false },
+    address: { type: String, required: false },
+    dateOfBirth: { type: String, required: false },
+    civilStatus: { type: String, required: false },
+    nationality: { type: String, required: false }, 
+    occupation: { type: String, required: false },
+    yrsOfResidency: { type: Number, required: false },
+    
+    purpose : { type: String, required: false },
+    documentNumber : { type: String, required: false },
+    dateIssued : { type: String, required: false },
+
+    businessName : { type: String, required: false },
+    businessAddress : { type: String, required: false },
+    businessType : { type: String, required: false },
+    businessNature : { type: String, required: false },
+   
+});
+
+export default mongoose.model('Documents', DocumentSchema)
